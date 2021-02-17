0b88d31b350c8af4d9e8ed5b233f9f3840d1fe0d
anilbhatt1 <anilaknb@gmail.com>
2021-02-17 09:43:45 +0530

    Moving Mobilenet initial version

--- main_app.py
+++ main_app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import torch
+#import gdown
+
+from io import BytesIO
+from PIL import Image
+from typing import Dict
+
+from utils import mobilenet_classify, local_css
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
 
 def main():
     """ EVA4-Phase 2 Deep Learning Models """
@@ -7,15 +17,25 @@ def main():
     st.text('')
     st.text('')
 
+    st.set_option('deprecation.showfileUploaderEncoding', False)
     #Mobilenet
     if st.checkbox("Predict flying objects"):
         st.subheader("Predicting flying objects using mobilenet")
+        flying_object_classify()        
 
-
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
+def flying_object_classify():
+     file: BytesIO = st.file_uploader("Upload an image file", type=["jpg", "png"])
+     if file is not None:
+#         url = 'https://drive.google.com/file/d/1-1-e-b2yFAu13t58rUsZoiG8u5c9MkL4'
+#         output = 'fo_model.pt'
+#         gdown.download(url, output, quiet=False)
+#         gdd.download_file_from_google_drive(file_id='1-1-e-b2yFAu13t58rUsZoiG8u5c9MkL4', dest_path='./fo_model.pt', unzip=False)
+#         download_file_from_google_drive('1-1-e-b2yFAu13t58rUsZoiG8u5c9MkL4', './fo_model_new.pt')
+#         model = torch.load('fo_model_new.pt')
+         model = torch.load('flyingobject_model.pt')
+         st.text('Model loaded successfully')
+         predicted = mobilenet_classify(model, file)
+         st.markdown(f'###Model identified uploaded image as {predicted}')
 
 if __name__ == '__main__':
     main()
---
+++ utils.py
@@ -0,0 +1,69 @@
+from io import BytesIO
+from typing import List, Tuple, Dict
+
+from torch import Tensor
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as T
+import streamlit as st
+import requests
+
+from PIL import Image
+from torchvision.transforms import Compose
+
+Mean: List = [0.48043839, 0.44820218, 0.39760034]
+STD: List = [0.27698959, 0.26908774, 0.28216029]
+CLASS_NAMES = ['Flying Birds', 'Large QuadCopters', 'Small QuadCopters', 'Winged Drones']
+
+TRANSFORMS: Compose = T.Compose([
+    T.Resize((224, 224)),
+    T.ToTensor(),
+    T.Normalize(mean=Mean,std=STD),
+    ])
+
+def mobilenet_classify(model: nn.Module, image: BytesIO):
+    img: Image  = Image.open(image).convert('RGB')
+    img: Tensor = TRANSFORMS(img)
+    img.unsqueeze_(0)
+
+    model.eval()
+    with torch.no_grad():
+        labels_pred: Tensor = model(img)
+        labels_pred_max  = labels_pred.argmax(dim =1, keepdim = True)
+
+    labels_pred_max = labels_pred_max.cpu().numpy()
+    return labels_pred_max
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+def download_file_from_google_drive(id, destination):
+    def get_confirm_token(response):
+        for key, value in response.cookies.items():
+            if key.startswith('download_warning'):
+                return value
+
+        return None
+
+    def save_response_content(response, destination):
+        CHUNK_SIZE = 32768
+
+        with open(destination, "wb") as f:
+            for chunk in response.iter_content(CHUNK_SIZE):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)
