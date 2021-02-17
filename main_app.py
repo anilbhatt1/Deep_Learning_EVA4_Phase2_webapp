9bcc557d363bb277b2398eb27302dfd37adcb166
anilbhatt1 <anilaknb@gmail.com>
2021-02-17 15:21:27 +0530

    modified main_app.py to change model path

--- main_app.py
+++ main_app.py
@@ -32,7 +32,7 @@ def flying_object_classify():
 #         gdd.download_file_from_google_drive(file_id='1-1-e-b2yFAu13t58rUsZoiG8u5c9MkL4', dest_path='./fo_model.pt', unzip=False)
 #         download_file_from_google_drive('1-1-e-b2yFAu13t58rUsZoiG8u5c9MkL4', './fo_model_new.pt')
 #         model = torch.load('fo_model_new.pt')
-         model = torch.load('flyingobject_model.pt')
+         model = torch.load('./flyingobject_model.pt')
          st.text('Model loaded successfully')
          predicted = mobilenet_classify(model, file)
          st.markdown(f'###Model identified uploaded image as {predicted}')
