0d5b4a390cc7a6186a48a9bb4613f7ba49448ba0
anilbhatt1 <anilaknb@gmail.com>
2021-03-19 15:05:09 +0530

    Modified styletransfre_process.py to directly download .pt files

--- src/styletransfer_process.py
+++ src/styletransfer_process.py
@@ -30,20 +30,38 @@ def generate(file):
         st.exception("ExceptionError('Please give an input image in valid format')")
 
 def style_process(style, file):
-    model_zip_path          = './fast_style_transfer_models_cpu.zip'
     model_candy_path        = './candy_cpu_scripted.pt'
     model_udnie_path        = './udnie_cpu_scripted.pt'
     model_mosaic_path       = './mosaic_cpu_scripted.pt'
     model_rainprincess_path = './rain_princess_cpu_scripted.pt'
 
     with st.spinner('Generating style image...'):
-        if not os.path.exists(model_zip_path):
+        '''
+         Commenting out downloading zip file as heroku app is facing time-out...
+
+         if not os.path.exists(model_zip_path):
             gdd.download_file_from_google_drive(file_id='1RclvV5Ep2c2BYxGDxQyEHFgnkcVQG8J8', dest_path='./fast_style_transfer_models_cpu.zip', unzip=False)
-            st.text('Model zip file downloaded')
+            st.text('Model zip file downloaded')         
 
             with ZipFile('./fast_style_transfer_models_cpu.zip', mode='r') as input:
                 input.extractall('.')
                 st.text('Models unzipped and downloaded')
+        '''
+        if not os.path.exists(model_candy_path):
+            gdd.download_file_from_google_drive(file_id='1zc_Y1EizuOoHd-v-dKmHJ-bEQq2HFjA2', dest_path='./candy_cpu_scripted.pt', unzip=False)
+            st.text('Candy model file downloaded')
+
+        if not os.path.exists(model_udnie_path):
+            gdd.download_file_from_google_drive(file_id='13iyRJC8vMblpMUORkXQL9eKOHXU3-XIu', dest_path='./udnie_cpu_scripted.pt', unzip=False)
+            st.text('Udnie model file downloaded')
+
+        if not os.path.exists(model_mosaic_path):
+            gdd.download_file_from_google_drive(file_id='1LFW1FyjC2MD_iMVBkCAeiA8eKhN-0JIw', dest_path='./mosaic_cpu_scripted.pt', unzip=False)
+            st.text('Mosaic model file downloaded')
+
+        if not os.path.exists(model_rainprincess_path):
+            gdd.download_file_from_google_drive(file_id='1LtFipvF8To2bMFhfAyRLHUpeUSkOC92N', dest_path='./rain_princess_cpu_scripted.pt', unzip=False)
+            st.text('Rain-Princess model file downloaded')
 
         pil_img     = Image.open(file).convert('RGB')
         content_img = content_transform(pil_img).unsqueeze(0)
